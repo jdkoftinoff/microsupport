5b92c224127cd3dd29599ded05b6630282c4013d
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2010-09-22 15:05:13 -0700

    nb project updated

---
+++ build/nb/us_test_all/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=us_test_all
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/us_test_all
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=ustestall.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/ustestall.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=us_test_all
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/us_test_all
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=ustestall.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/ustestall.tar
