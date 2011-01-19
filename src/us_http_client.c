e35ec9083e84e092eee42089409962ef9c8d76d4
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2011-01-18 21:57:34 -0800

    h,c files added for http client and server

--- src/us_http_client.c
+++ src/us_http_client.c
@@ -1,12 +1,3 @@
-#include "us_world.h"
-#include "us_allocator.h"
-#include "us_buffer.h"
-#include "us_net.h"
-#include "us_http.h"
-#include "us_getopt.h"
-#include "us_logger_printer.h"
-
-#include "us_testutil.h"
 
 /*
  Copyright (c) 2010, Meyer Sound Laboratories, Inc.
@@ -19,14 +10,14 @@
  * Redistributions in binary form must reproduce the above copyright
  notice, this list of conditions and the following disclaimer in the
  documentation and/or other materials provided with the distribution.
- * Neither the name of Meyer Sound Laboratories, Inc. nor the
+ * Neither the name of the Meyer Sound Laboratories, Inc. nor the
  names of its contributors may be used to endorse or promote products
  derived from this software without specific prior written permission.
 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- DISCLAIMED. IN NO EVENT SHALL MEYER SOUND LABORATORIES, INC.  BE LIABLE FOR ANY
+ DISCLAIMED. IN NO EVENT SHALL MEYER SOUND LABORATORIES, INC. BE LIABLE FOR ANY
  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -35,41 +26,6 @@
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include "us_world.h"
 
-/** \addtogroup us_test_net */
-/*@{*/
-
-
-bool us_example_httpd ( int argc, char **argv );
-
-bool us_example_httpd ( int argc, char **argv )
-{
-    bool r = false;
-    return r;
-}
-
-int main ( int argc, char **argv )
-{
-    int r = 1;
-    if ( us_testutil_start ( 4096, 4096, argc, argv ) )
-    {
-#if US_ENABLE_LOGGING
-        us_logger_printer_start ( us_testutil_printer_stdout, us_testutil_printer_stderr );
-#endif
-        us_log_set_level ( US_LOG_LEVEL_DEBUG );
-        us_log_info ( "Hello world from %s compiled on %s", __FILE__, __DATE__ );
-        if ( us_example_httpd ( argc, argv ) )
-            r = 0;
-        us_log_info ( "Finishing %s", argv[0] );
-        us_logger_finish();
-        us_testutil_finish();
-    }
-    return r;
-}
-
-
-/*@}*/
-
-
-
-
+#include "us_http_client.h"
