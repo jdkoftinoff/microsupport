40c01f9d456b2bc0b7bd32fc5ce361d0403df3ad
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-12-17 17:07:21 -0800

    us_ms*.h added

--- include/us_world.h
+++ include/us_world.h
@@ -107,18 +107,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <process.h>
 
-typedef long ssize_t;
-
-#if !defined(_STDINT_H)
-typedef long long int64_t;
-typedef unsigned long long uint64_t;
-typedef int int32_t;
-typedef unsigned int uint32_t;
-typedef short int16_t;
-typedef unsigned short uint16_t;
-typedef char int8_t;
-typedef unsigned char uint8_t;
-#endif
+#include "us_ms.h"
 
 #endif
 
