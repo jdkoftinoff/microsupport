863dc84eb54f2157351b6b4dce1b88b29955221a
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2014-01-13 21:56:59 -0800

    year is 2014

--- src/us_rawnet_multi.c
+++ src/us_rawnet_multi.c
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2012, J.D. Koftinoff Software, Ltd.
+ Copyright (c) 2014, J.D. Koftinoff Software, Ltd.
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
